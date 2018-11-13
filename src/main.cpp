6b59e7dea056be8b04051b7f2bd6af0683e84feb
winding345 <winding345@gmail.com>
2018-11-13 19:31:43 +0800

    modify

--- src/main.cpp
+++ src/main.cpp
@@ -33,8 +33,7 @@ int main(int argc,char *argv[])
     while(i--)
     {
         std::cin>>input;
-        sprintf(itc,"%d.bmp",i);
-        std::cout<<input<<" "<<itc<<std::endl;
+        sprintf(itc,"%d",input);
         if(input == -1)
             break;
         if(input == -2)
